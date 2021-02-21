9028501a8b053d9eb6c746da1607179b0721250e
Jesús Antonio García Valadez <jesus.garciav@me.com>
2021-02-21 17:19:22 -0600

    feat: remove previous layout organization

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,7 @@
   <div id="app">
     <Navigation />
 
-    <main class="-mt-32">
-      <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-        <div class="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
-          <div class="min-h-96">
-            <Nuxt />
-          </div>
-        </div>
-      </div>
-    </main>
+    <Nuxt />
 
     <Footer />
   </div>
